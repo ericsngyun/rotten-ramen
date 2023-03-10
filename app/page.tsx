060de53498cab4249b159a09568ffc2cb4625963
Eric Yun <yunseric@gmail.com>
2023-03-10 10:54:49 -0800

    added navbar, and navigation components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Image from 'next/image'
-
+import Nav from '@/components/navbar/Nav'
 
 export default function Home() {
   return (
     <main>
-      <div>
-        home
-      </div>
+      
     </main>
   )
 }
---
+++ components/navbar/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const Nav = (props: Props) => {
+  return (
+    <div className = "flex justify-between ">
+        <h1 className = "logo text-2xl text-[#18181c]">
+          rotten-ramen
+        </h1>
+    </div>
+  )
+}
+
+export default Nav
