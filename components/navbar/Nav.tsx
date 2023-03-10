fc9bd3e50d87fb20ab08b7cebe0889b85de905a5
Eric Yun <yunseric@gmail.com>
2023-03-10 11:51:11 -0800

    nav changes, added prisma

--- components/navbar/Nav.tsx
+++ components/navbar/Nav.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Button } from '../ui/Button'
 
 type Props = {}
 
 const Nav = (props: Props) => {
   return (
-    <div className = "flex justify-between ">
+    <div className = "flex justify-between px-8 py-4">
         <h1 className = "logo text-2xl text-[#18181c]">
           rotten-ramen
         </h1>
+        <div >
+          <Button>
+            Sign Up
+          </Button>
+          <Button variant = "link">
+            Sign In
+          </Button>
+        </div>
     </div>
   )
 }
